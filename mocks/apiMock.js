49d37f88dcf6a08905eb7d63d803e98b6318d7db
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-17 21:19:52 +0200

    test: mocks-server collections added

--- mocks/apiMock.js
+++ mocks/apiMock.js
@@ -8,4 +8,4 @@ const { loadRoutes, loadCollections } = server.mock.createLoaders();
 loadRoutes(awsAPIPickup);
 loadCollections(collections)
 
-await server.mock.collections.select("base");
+await server.mock.collections.select("ok");
--- mocks/collections.js
+++ mocks/collections.js
@@ -1,6 +1,14 @@
 export default [
   {
-    "id": "base",
+    "id": "ok",
     "routes": ["get-pickup:ok"]
-  }
+  },
+  {
+    "id": "wrongUrlParam",
+    "routes": ["get-pickup:wrongly-formatted-urlparam"]
+  },
+  {
+    "id": "nonExistingStreet",
+    "routes": ["get-pickup:non-existing-street"]
+  },
 ]
