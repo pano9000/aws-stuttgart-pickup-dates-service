7e060a3c4c4f27c582acee5cdf4707a2cdac1d70
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-17 21:25:45 +0200

    server/api/next: update controller validation and AwsAPIClient changes

--- server/api/v1/next.get.ts
+++ server/api/v1/next.get.ts
@@ -1,30 +1,15 @@
-import AwsAPIClient from "../../services/AwsAPIClient";
+import { awsAPIClient } from "~/server/services/AwsAPIClient";
+import { schemaQuery } from "~/server/validationSchemas/schemaQuery";
+//export default defineEventHandler({onRequest: [validateSchema(schemaQuery)], handler: async (event) => {
 
 export default defineEventHandler(async (event) => {
-  //TODO add middleware for validation of query params before this step
   try {
     const query = getQuery(event);
-    const awsAPIClient = new AwsAPIClient().setAddress(query.streetname as string, query.streetno as string);
-    const data = await awsAPIClient.getUpcoming();
-
-    return data
-
-  } catch(error) {
-    //TODO replace by propper logger
-    console.error(error);
-    setResponseStatus(event, 500)
-    return {
-      error: "Unexpected Server Error"
-    }
-
-    /* this returns HTML for some reason -> to be investigated -> is this a bug or PEBKAC :-)?
-    throw createError({
-      status: 500,
-      data: {
-        error: "Unexpected Server Error"
-      }
-    })
-    */
+    const validatedQuery = schemaQuery.parse(query);
+    const data = await awsAPIClient.getUpcoming(validatedQuery.streetname, validatedQuery.streetno);
+    return data;
   }
-
+  catch(error) {
+    errorHandler(error);
+  }
 })
