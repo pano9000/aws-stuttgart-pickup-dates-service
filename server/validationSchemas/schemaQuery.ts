94e9982628b645ddea877ab633ec4bbd50a3954b
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-17 22:05:44 +0200

    server: add "format" as prop to query schema
    defaulting to "JSON"

--- server/validationSchemas/schemaQuery.ts
+++ server/validationSchemas/schemaQuery.ts
@@ -2,5 +2,6 @@ import { z } from "zod";
 
 export const schemaQuery = z.object({
   streetname: z.string().max(150),
-  streetno: z.string().max(50)
+  streetno: z.string().max(50),
+  format: z.enum(["json", "ical", "csv"]).default("json")
 })
