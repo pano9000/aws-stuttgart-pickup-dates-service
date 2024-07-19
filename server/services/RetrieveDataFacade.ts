8f5b81a56c9088a3e48729b715c6f35d3a74a6d3
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-19 22:37:03 +0200

    (server): add RetrieveDataFacade for easier data store/retrieval
    - tries to get value from redis,
    - if it does not exist -> hits the AWSApi, then stores the transformed data in redis and returns the value from redis

---
+++ server/services/RetrieveDataFacade.ts
@@ -0,0 +1,38 @@
+import type AwsApiService from "./AwsApiService";
+import { awsApiService as defaultAwsApiService } from "./AwsApiService";
+import { RedisService, default as defaultRedisService } from "./RedisService";
+
+
+export class RetrieveDataFacade {
+
+  awsApiService: AwsApiService;
+  redisService: RedisService;
+
+  constructor(awsApiService: AwsApiService = defaultAwsApiService, redisService: RedisService = defaultRedisService) {
+    this.awsApiService = awsApiService;
+    this.redisService = redisService;
+  }
+
+  #createRedisKey(streetname: string, streetno: string) {
+    return `address_${streetname}|${streetno}`.toLowerCase();
+  }
+
+  async getAll(streetname: string, streetno: string, operationId: string = ""): Promise<any> {
+    const redisKey = this.#createRedisKey(streetname, streetno);
+    const redisResult = await this.redisService.jsonGET(redisKey)
+
+    if (!redisResult) {
+      const awsApiData = await this.awsApiService.getAll(streetname, streetno);
+      const redisSave = await this.redisService.jsonSET(redisKey, data); // this throws, if saving was not succesful
+      if (redisSave !== "OK") throw new Error("saving to redis failed, aborting!")
+      //@TODO set expiration date - so that refetch is forced 
+      return await this.getAll(streetname, streetno);
+    }
+    if (typeof redisResult !== "string") throw new Error("DB did not return a valid string")
+    //@TODO try/catch JSON parse?
+    return JSON.parse(redisResult);
+  }
+
+}
+
+export const retrieveDataFacade = new RetrieveDataFacade();
