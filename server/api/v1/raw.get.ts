4ee3fc3667641530eced3737208b50761fe68be6
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-24 08:29:10 +0200

    (server/api): use updated awsApiService in `raw.get` route

--- server/api/v1/raw.get.ts
+++ server/api/v1/raw.get.ts
@@ -1,4 +1,4 @@
-import { awsAPIClient } from "~/server/services/AwsAPIClient";
+import { awsApiService } from "~/server/services/AwsApiService";
 import { schemaQuery } from "~/server/validationSchemas/schemaQuery";
 //export default defineEventHandler({onRequest: [validateSchema(schemaQuery)], handler: async (event) => {
 
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
   try {
     const query = getQuery(event);
     const validatedQuery = schemaQuery.parse(query);
-    const data = await awsAPIClient.getRaw(validatedQuery.streetname, validatedQuery.streetno);
+    const data = await awsApiService.getRaw(validatedQuery.streetname, validatedQuery.streetno);
     return data;
   }
   catch(error) {
