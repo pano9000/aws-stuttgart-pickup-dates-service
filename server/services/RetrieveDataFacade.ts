be10ad2f15795fd2125fbea08a788d4c95a6f1b5
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-25 19:32:43 +0200

    (server/RetrieveDataFacade): refactor refetchFromAwsApi to own method

--- server/services/RetrieveDataFacade.ts
+++ server/services/RetrieveDataFacade.ts
@@ -18,16 +18,30 @@ export class RetrieveDataFacade {
     const redisResult = await this.redisService.jsonGET(redisKey)
 
     if (!redisResult) {
-      const awsApiData = await this.awsApiService.getAll(streetname, streetno);
-      const redisSave = await this.redisService.jsonSET(redisKey, awsApiData); // this throws, if saving was not succesful
-      if (redisSave !== "OK") throw new Error("saving to redis failed, aborting!")
-      //@TODO set expiration date - so that refetch is forced 
+      await this.#refetchFromAwsApi(streetname, streetno);
       return await this.getAll(streetname, streetno);
     }
+
     if (typeof redisResult !== "string") throw new Error("DB did not return a valid string")
     //@TODO try/catch JSON parse?
     return JSON.parse(redisResult);
   }
+
+    // Fetch Full Data from AWS Stuttgart API and store in our redis DB, throws an error if anything goes wrong in any step
+    async #refetchFromAwsApi(streetname: string, streetno: string, operationId: string = ""): Promise<void> {
+      try {
+        const redisKey = this.redisService.getRedisKey(streetname, streetno);
+        const awsApiData = await this.awsApiService.getAll(streetname, streetno);
+        const redisSave = await this.redisService.jsonSET(redisKey, awsApiData); // this throws, if saving was not succesful
+        //@TODO set expiration date - so that refetch is forced 
+        if (redisSave !== "OK") throw new Error("saving to redis failed, aborting!")
+      }
+      catch(error) {
+        //@TODO handle errors?
+        console.error("Refetch failed");
+        throw error;
+      }
+    }
 
 }
 
