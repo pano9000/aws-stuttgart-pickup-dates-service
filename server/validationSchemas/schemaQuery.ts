26a5dabb9899fcc2ea6ac8063b82c2668b209f3d
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-28 15:17:43 +0200

    (server/schemaQuery): adjust max length of streetname

--- server/validationSchemas/schemaQuery.ts
+++ server/validationSchemas/schemaQuery.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const schemaQuery = z.object({
-  streetname: z.string().max(150),
+  streetname: z.string().max(100),
   streetno: z.string().max(50),
   format: z.enum(["json", "ical", "csv"]).default("json")
 })
