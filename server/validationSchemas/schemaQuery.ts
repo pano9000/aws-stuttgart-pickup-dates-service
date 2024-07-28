82512d3de3831c2b28feef1d8e04cfaef4a28b83
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-28 15:20:44 +0200

    (server/schemaQuery): add support for comma separated 'type' query param
    when param is empty defaults to undefiend -> which will include 'all' types
    
    when param is entered returns a deduped array of types for further filtering

--- server/validationSchemas/schemaQuery.ts
+++ server/validationSchemas/schemaQuery.ts
@@ -1,7 +1,15 @@
 import { z } from "zod";
+import { SchemaAwsApiServiceEventTypeName } from "~/server/services/AwsApiService"
 
 export const schemaQuery = z.object({
   streetname: z.string().max(100),
   streetno: z.string().max(50),
   format: z.enum(["json", "ical", "csv"]).catch("json"),
+  type: z.string().optional()
+    .transform(str => {
+      return (!str)
+        ? undefined
+        : Array.from(new Set(str.split(",").map(splitVal => splitVal.trim())))
+    })
+    .pipe(z.array(SchemaAwsApiServiceEventTypeName).optional()).catch(undefined)
 });
