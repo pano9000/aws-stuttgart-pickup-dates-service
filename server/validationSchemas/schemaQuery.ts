009ed4849960f60b351dd9782bf2c8bf0d7598af
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-28 15:19:01 +0200

    (server/schemaQuery): always revert back to format "json", if invalid or empty value is provided

--- server/validationSchemas/schemaQuery.ts
+++ server/validationSchemas/schemaQuery.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const schemaQuery = z.object({
   streetname: z.string().max(100),
   streetno: z.string().max(50),
-  format: z.enum(["json", "ical", "csv"]).default("json")
-})
+  format: z.enum(["json", "ical", "csv"]).catch("json"),
+});
