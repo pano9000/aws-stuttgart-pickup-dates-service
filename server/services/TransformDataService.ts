cacb519ea90e6d36d6344a6e49bb80ffbf8d0ee5
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-07-30 23:26:21 +0200

    (server/TransformDataService): tidy up code comments

--- server/services/TransformDataService.ts
+++ server/services/TransformDataService.ts
@@ -5,23 +5,19 @@ export class TransformDataService {
 
 
   static toCSV(originalData: AwsApiServiceResponseAll): string {
-  //@TODO investigate if LF/CR could be come an issue?
-
-  /**
-   * date,type,schedule,irregularSchedule,streetname,streetno
-   */
-  const csvHeader = `data,type,schedule,irregularSchedule,streetname,streetno\r\n`;
-  const csvLines = originalData.data.map((event) => {
+    //@TODO investigate if LF/CR could be come an issue?
+    const csvHeader = `data,type,schedule,irregularSchedule,streetname,streetno\r\n`;
     //@TODO investigate if we need to wrap values in double quotes -> not sure if streetname/number might cause issues, if not
-    return [event.date, event.type, event.schedule, event.irregularSchedule, originalData.information.streetname, originalData.information.streetno].join(",")
-  })
+    const csvLines = originalData.data.map((event) => {
+      return [event.date, event.type, event.schedule, event.irregularSchedule, originalData.information.streetname, originalData.information.streetno].join(",")
+    })
 
-  return csvHeader + csvLines.join("\r\n")
+    return csvHeader + csvLines.join("\r\n")
 
-}
+  }
 
-static toICal(originalData: AwsApiServiceResponseAll): string {
-  throw new Error("TODO: implement");
-}
+  static toICal(originalData: AwsApiServiceResponseAll): string {
+    throw new Error("TODO: implement");
+  }
 
 }
