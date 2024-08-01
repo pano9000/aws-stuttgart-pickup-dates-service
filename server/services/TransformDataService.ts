a440a0d0fc89776b6fcca57b04deb56de1820cef
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-01 09:15:10 +0200

    (server/TransformDataService): add initial toICal options implementation

--- server/services/TransformDataService.ts
+++ server/services/TransformDataService.ts
@@ -1,5 +1,5 @@
 import type { AwsApiServiceResponseAll } from "./AwsApiService"
-import ical from 'ical-generator';
+import ical, { ICalEventTransparency } from 'ical-generator';
 import { DateTime } from "luxon";
 
 //@TODO: error handling?
@@ -16,7 +16,7 @@ export class TransformDataService {
     return csvHeader + csvLines.join("\r\n")
   }
 
-  static toICal(originalData: AwsApiServiceResponseAll): string {
+  static toICal(originalData: AwsApiServiceResponseAll, options?: ICalOptions): string {
 
     const { streetname, streetno } = originalData.information;
     const calendar = ical({
@@ -24,25 +24,56 @@ export class TransformDataService {
       timezone: "Europe/Berlin"
     })
 
-    const createDate = (dateString: string, hour: number, minute: number) => {
-      const date = DateTime.fromISO(dateString, {zone: "Europe/Berlin"}).set({hour, minute});
-      if (!date.isValid) {
-        throw new Error("Unable to create a valid date")
+    const createDate = (dateString: string, time: HourMinuteTuple) => {
+      const date = DateTime.fromISO(dateString, {zone: "Europe/Berlin"}).set({hour: time[0], minute: time[1]});
+      if (!date.isValid) throw new Error("Unable to create a valid date")
+
+      if (options?.offsetEvent && options.offsetEvent > 0) {
+        return date.minus({ days: options.offsetEvent})
       }
+
       return date
+    }
+
+    const createSummary = (eventType: string, eventSchedule: string) => {
+      if (options?.customSummary) {
+        //@TODO: strip any html? or vcalendar tags
+        return options.customSummary.replaceAll("%1", eventType).replaceAll("%2", eventSchedule )
+      }
+      return `AWS Pickup ${eventType} (${eventSchedule}) ${(options?.offsetEvent) ? 'offset' : ''}`
     }
 
     originalData.data.forEach(event => {
 
-      calendar.createEvent({
-        start: createDate(event.date, 6, 30),
-        end: createDate(event.date, 7, 0),
-        summary: `AWS Pickup ${event.type} (${event.schedule})`,
+      const currentEvent = calendar.createEvent({
+        start: createDate(event.date, options?.startTime || [6, 30]),
+        end: createDate(event.date, options?.startTime || [7, 0]),
+        summary: createSummary(event.type, event.schedule),
+        //description: `AWS Pickup ${event.type} (${event.schedule}) ${(options?.offsetEvent) ? 'offset' : ''}`,
         location: `${streetname} ${streetno}`,
+        transparency: ICalEventTransparency.TRANSPARENT,
+        allDay: options?.allDay
       })
+
+      if (options?.alarm && Number.isInteger(options.alarm)) {
+        currentEvent.createAlarm({trigger: options.alarm})
+      }
+
+
     })
 
     return calendar.toString()
   }
 
+}
+
+
+type HourMinuteTuple = [hour: number, minute: number];
+export type ICalOptions = {
+  startTime?: HourMinuteTuple; // e.g. [6, 30]
+  endTime?: HourMinuteTuple; // e.g. [7, 15]
+  allDay?: boolean;
+  alarm?: number // e.g. 600 -> 10 min before
+  offsetEvent?: number // e.g. 1 -> for offsetting event by one day
+  customSummary?: string // e.g. 'Abholung Müll %1 (%2)' - where %1 is the type and %2 is the schedule
 }
