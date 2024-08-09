9aeb2eb629eec71bf1f27334878569f5dde90424
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-09 08:38:28 +0200

    (components): add TimeRangeInput custom input component

---
+++ components/TimeRangeInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="timerangeinput__wrap">
+    <v-icon v-if="props.icon" :icon="props.icon"></v-icon>
+    <input
+      v-model="modelStartTime"
+      type="time"
+    >
+    —
+    <input
+      v-model="modelEndTime"
+      type="time"
+    >
+  </div>
+
+</template>
+
+
+<script setup lang="ts">
+
+  const modelStartTime = defineModel<string>("startTime");
+  const modelEndTime = defineModel<string>("endTime");
+
+  const props = defineProps<{
+    icon?: string
+  }>();
+
+</script>
+
+<style scoped>
+.timerangeinput__wrap input {
+  border: 1px solid black; 
+  border-radius: 5px; /*@TODO: check how to use same style as vuetify here*/
+}
+
+.timerangeinput__wrap input:hover {
+  cursor: text;
+  background-color: gainsboro; /*@TODO: check how to use same style as vuetify here*/
+}
+</style>
