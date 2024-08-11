cbe17c58afd38821698ba04f22915a63c03c59e9
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-11 14:19:12 +0200

    (components/NumberInput): add new custom component

---
+++ components/NumberInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="numberinput__wrap">
+    <v-icon v-if="props.icon" :icon="props.icon" class="mr-2"></v-icon>
+    <input
+      type="number"
+      class="mr-2"
+      :id=id
+      :min=min
+      :max=max
+      :step=step
+      :disabled="disabled"
+      v-model.lazy="modelInputNumber"
+    >
+    <label :for="id">{{ label }}</label>
+
+  </div>
+
+</template>
+
+
+<script setup lang="ts">
+  //@TODO RTL support for label in template 
+  const modelInputNumber = defineModel<number>("inputNumber");
+
+
+  const props = defineProps<{
+    icon?: string,
+    disabled?: boolean,
+    min?: number,
+    max?: number,
+    step?: number,
+    label?: string,
+    id?: string
+  }>();
+
+</script>
+
+<style scoped>
+
+.numberinput__wrap {
+  min-height: var(--v-input-control-height);
+  align-content: center;
+}
+
+.numberinput__wrap input {
+  border: 1px solid black; 
+  border-radius: 5px; /*@TODO: check how to use same style as vuetify here*/
+}
+
+.numberinput__wrap input:hover {
+  cursor: text;
+  background-color: gainsboro; /*@TODO: check how to use same style as vuetify here*/
+}
+
+.wat input {
+  background-color: yellow;
+}
+</style>
