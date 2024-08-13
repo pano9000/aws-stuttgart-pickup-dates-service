f0cae7078fbdf19f301bf3304b417da5f2c25bc8
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-13 09:21:57 +0200

    (components/LocationPicker): WIP use Combobox for displaying suggestions

--- components/LocationPicker.vue
+++ components/LocationPicker.vue
@@ -9,7 +9,7 @@
   >
     <span class="font-weight-bold">Current Address:</span>
 
-    <v-text-field 
+    <!-- <v-text-field 
       v-model="streetname"
       label="Street Name"
       clearable
@@ -22,6 +22,26 @@
       clearable
       prepend-icon="mdi-numeric"
     />
+    
+    -->
+
+    <v-combobox
+      v-model="streetname"
+      label="Street Name "
+      :items="streetnameSuggestions"
+      prepend-icon="mdi-home-city"
+      clearable
+    />
+
+    <v-combobox
+      v-model="streetno"
+      label="Street Number"
+      :items="streetnoSuggestions"
+      prepend-icon="mdi-numeric"
+      clearable
+    />
+
+
 
     <v-btn
       v-if="locationPickerActive"
@@ -47,13 +67,15 @@
 
 <script setup lang="ts">
 import { useCookieUserConfig } from '~/composables/useCookieUserConfig';
-
+import { watchDebounced } from '@vueuse/core'
 
 const { cookieStreet } = useCookieUserConfig();
 const streetname = ref(cookieStreet.value.streetname);
 const streetno = ref(cookieStreet.value.streetno);
 const locationPickerActive = ref(false);
 
+const streetnameSuggestions = ref<string[]>([]);
+const streetnoSuggestions = ref<string[]>([]);
 
 //@TODO: how to get cookieStreet as argument? it fails due to type, beacuse in template it is "unpacked"
 function storeStreetInCookie(streetname: string, streetno: string) {
@@ -61,6 +83,45 @@ function storeStreetInCookie(streetname: string, streetno: string) {
   cookieStreet.value.streetno = streetno;
   locationPickerActive.value = false
 }
+
+watchDebounced(
+  streetname, 
+  async (newStreetname, _oldStreetname) => {
+    streetno.value = ""
+    if (!newStreetname || newStreetname.length < 1) {
+      streetnameSuggestions.value = []
+      return;
+    }
+    const response = await $fetch("/api/v1/addresssuggestion", {
+      query: {
+        streetname: newStreetname
+      }
+    })
+    console.log(response);
+  streetnameSuggestions.value = response || []
+  },
+  { debounce: 1000, maxWait: 2000 },
+)
+
+watchDebounced(
+  streetno, 
+  async (newStreetno, _oldStreetno) => {
+    if (!newStreetno || newStreetno.length < 1) {
+      streetnoSuggestions.value = []
+      return;
+    }
+    const response = await $fetch("/api/v1/addresssuggestion", {
+      query: {
+        streetname: streetname.value,
+        streetno: newStreetno
+      }
+    })
+    console.log(response);
+    streetnoSuggestions.value = response || []
+
+  },
+  { debounce: 500, maxWait: 1000 },
+)
 
 </script>
 
