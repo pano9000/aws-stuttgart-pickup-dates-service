3c8b4b67f729fb7763df8e0680dc9b9a1f9e8fc3
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-16 07:19:55 +0200

    feat(logger): add winstonLogger config

---
+++ server/utils/winstonLogger.ts
@@ -0,0 +1,44 @@
+import winston from "winston";
+
+
+const reorderLogProps = winston.format((logData) => {
+  // set the exact order of the props here
+  const propKeys = ["operationId", "level", "service", "message", "timestamp"];
+  const { level, message, timestamp, service, operationId } = logData;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const logCopy: { [key: string]: any }= { level, message, timestamp, service, operationId };
+
+  for (const propKey of propKeys) {
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+    delete logData[propKey];
+    logData[propKey] = logCopy[propKey];
+  }
+  return logData;
+});
+
+const defaultFormat = winston.format.combine(
+  winston.format.timestamp(),
+  reorderLogProps(),
+  winston.format.json({deterministic: false})
+);
+
+export const generalLogger = winston.createLogger({
+    //level: "info",
+    format: defaultFormat,
+    transports: [
+      //new winston.transports.File({ filename: "./logs/all.log", maxsize: 10000000 , tailable: true, maxFiles: 999999 }),
+      new winston.transports.Console({level: "debug"})
+    ],
+    //exceptionHandlers: [ new winston.transports.File({ filename: "./logs/exceptions.log"}) ],
+    //rejectionHandlers: [ new winston.transports.File({ filename: "./logs/rejections.log"}) ],
+});
+
+export const httpLogger = winston.createLogger({
+    level: "http",
+    defaultMeta: { service: "server.http" },
+    format: defaultFormat,
+    transports: [ 
+      //new winston.transports.File({ filename: "./logs/http.log", level: "http", maxsize: 10000000 , tailable: true, maxFiles: 999999 }),
+      new winston.transports.Console({level: "http"})
+    ],
+});
