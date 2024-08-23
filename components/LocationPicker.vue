2ee7ba467d36c99e35d007018002f3cc1c7b2732
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-23 09:01:07 +0200

    feat(LocationPicker): turn into VMenu

--- components/LocationPicker.vue
+++ components/LocationPicker.vue
@@ -1,41 +1,56 @@
 <template>
-<div class="pa-4 bg-orange">
-  <v-form 
-    ref="form"
-    class="d-flex align-baseline ga-4"
-    :readonly="!isLocationPickerActive"
-    @submit.prevent="storeStreetInCookieHandler"
+  <v-menu
+    id="menu-locationpicker"
+    width="100vw"
+    :close-on-content-click="false"
+    open-on-hover
+    eager
   >
-    <v-row
-      align="center"
+    <template #activator="{ props }">
+      <v-btn 
+        icon="mdi-office-building-marker"
+        v-bind="props"
+        :color="(!hasSetStreet) ? 'red' : undefined"
+      />
+    </template>
+
+    <v-card 
+      class="pa-8"
     >
-      <v-col cols="5">
-        <LocationPickerComboBox
-          v-model:input-combo-box="streetname"
-          v-model:streetname="streetname"
-          v-model:streetno="streetno"
-          :is-enabled="isLocationPickerActive"
-          mode="streetname"
-          no-data-text="Street Name"
-          icon="mdi-home-city"
-          label="Street Name"
-        />
-      </v-col>
+      <v-form 
+        ref="form"
+        class="d-flex align-baseline ga-4"
+        :disabled="!isLocationPickerActive"
+        @submit.prevent="storeStreetInCookieHandler"
+      >
+        <v-row align="center">
+          <v-col cols="5">
+            <LocationPickerComboBox
+              v-model:input-combo-box="streetname"
+              v-model:streetname="streetname"
+              v-model:streetno="streetno"
+              :is-enabled="isLocationPickerActive"
+              mode="streetname"
+              no-data-text="Street Name"
+              icon="mdi-home-city"
+              label="Street Name"
+            />
+          </v-col>
 
-      <v-col cols="5">
-        <LocationPickerComboBox
-          v-model:input-combo-box="streetno"
-          v-model:streetname="streetname"
-          v-model:streetno="streetno"
-          :is-enabled="isLocationPickerActive"
-          mode="streetno"
-          no-data-text="Street Number"
-          icon="mdi-numeric"
-          label="Street Number"
-        />
-      </v-col>
+          <v-col cols="5">
+            <LocationPickerComboBox
+              v-model:input-combo-box="streetno"
+              v-model:streetname="streetname"
+              v-model:streetno="streetno"
+              :is-enabled="isLocationPickerActive"
+              mode="streetno"
+              no-data-text="Street Number"
+              icon="mdi-numeric"
+              label="Street Number"
+            />
+          </v-col>
 
-      <v-col cols="auto">
+          <v-col cols="auto">
             <v-tooltip
               :text="(isLocationPickerActive) ? `Save` : `Edit`"
               location="top"
@@ -59,16 +74,17 @@
               </template>
             </v-tooltip>
           </v-col>
-    </v-row>
-  </v-form>
-</div>
+        </v-row>
+      </v-form>
+    </v-card>
+  </v-menu>
 </template>
 
 <script setup lang="ts">
 import { VForm } from 'vuetify/components';
 import { useCookieUserConfig } from '~/composables/useCookieUserConfig';
 
-const { cookieStreet } = useCookieUserConfig();
+const { cookieStreet, hasSetStreet } = useCookieUserConfig();
 
 const streetname = ref(cookieStreet.value.streetname);
 const streetno = ref(cookieStreet.value.streetno);
