08790547fda79a84e617a64a2f1cd3b84581a676
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-24 00:12:53 +0200

    feat(app): display footer

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
-    <v-app>
-      <NavigationBase/>
-      <v-main>
-        <NuxtPage />
-      </v-main>
-
-    </v-app>
+  <v-app>
+    <NavigationBase/>
+    <!-- max-width="1280px" -->
+    <v-main
+    >
+      <NuxtPage />
+    </v-main>
+    <Footer/>
+  </v-app>
 </template>
