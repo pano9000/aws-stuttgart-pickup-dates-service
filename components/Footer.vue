158a332d8f0926118064486c5d1714460dfef78e
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-24 00:10:02 +0200

    feat: add prliminary footer

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-footer 
+    color="secondary"
+    class="elevation-8 flex-0-1"
+  >
+  <v-container>
+    <v-row  class="text-center">
+      <v-spacer cols="3"/>
+      <v-col cols="6">
+        2024 — Panagiotis Papadopoulos (@pano9000)
+      </v-col>
+      <v-col cols="3">
+        <v-tooltip
+          :text="'Check Sourcecode on GitHub'"
+        >
+          <template #activator="{ props }">
+            <a
+              href="https://github.com/pano9000/aws-stuttgart-pickup-dates-service"
+              target="_blank"
+              v-bind="props"
+            >
+              <v-icon icon="mdi-github"/>
+            </a>
+            </template>
+        </v-tooltip>
+
+      </v-col>
+    </v-row>
+  </v-container>
+
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
