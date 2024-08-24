aa503a1ab9f09f52fe4d82d26b0a22aaa42372ef
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-24 22:48:38 +0200

    feat(pages/all): add all page
    needs to be refactored later, to become more DRY,
    almost a dupe of page "upcoming"

---
+++ pages/all.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <h1>All Pickups</h1>
+    <p>All of the pickups for {{ `${streetname} ${streetno}` }}</p>
+
+    <BaseEventDisplay :event-data="apiData"/>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+  import type { AwsApiServiceResponseAll } from "~/server/services/AwsApiService.js"
+  import { useCookieUserConfig } from "~/composables/useCookieUserConfig";
+  import BaseEventDisplay from "~/components/EventDisplay/BaseEventDisplay.vue";
+
+
+  const { cookieStreet } = useCookieUserConfig();
+  const streetname = toRef(() => cookieStreet.value.streetname);
+  const streetno = toRef(() => cookieStreet.value.streetno);
+
+  //@TODO https://nuxt.com/docs/guide/recipes/custom-usefetch#custom-usefetch
+  //@TODO - fix empty api call, when no streetname/streetno is set
+  const { data: apiData, status, error, refresh, clear } = await useFetch<AwsApiServiceResponseAll>("/api/v1/all", {
+    query: {
+      streetname: streetname,
+      streetno: streetno
+    },
+  });
+
+
+</script>
