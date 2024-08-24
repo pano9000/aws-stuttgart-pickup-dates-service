491382f90d9a04fff4b26740d066944fe9aea4c0
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-24 18:48:37 +0200

    feat(MenuLanguage): add flag icons
    flag icons from https://github.com/lipis/flag-icons (MIT Licensed)
    
    @TODO need to find the best way to credit the license

--- components/Navigation/NavigationAppBarMenuLanguage.vue
+++ components/Navigation/NavigationAppBarMenuLanguage.vue
@@ -1,24 +1,33 @@
 <template>
-   <v-menu
-      open-on-hover
-    >
-      <template #activator="{ props }">
-        <v-btn
-          v-bind="props"
-          icon="mdi-translate-variant"
-        />
-      </template>
+  <v-menu
+    open-on-hover
+    eager
+  >
+    <template #activator="{ props }">
+      <v-btn
+        v-bind="props"
+        icon="mdi-translate-variant"
+      />
+    </template>
 
-      <v-list>
-        <v-list-item
-          v-for="language in languages"
-          :key="language"
-        >
-          <!-- <v-btn>{{ language }}</v-btn> -->
-           {{ language }}
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <v-list>
+      <v-list-item
+        v-for="language in languages"
+        :key="language"
+      >
+      <template #default>
+        <div class="d-flex ga-2 align-center">
+          <v-img
+            :src="`/img/flag_${language}.svg`"
+            min-width="2rem"
+            inline
+          />
+          <span>{{ language }}</span>
+        </div>
+      </template>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script setup lang="ts">
