3945eea3c5c48a2ab4411f7e4a596321c99ab692
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-24 22:39:14 +0200

    feat(EventDisplay): add EventDisplay components
    enables users to switch between views

---
+++ components/EventDisplay/EventDisplayCalendar.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-alert>Calendar view #TODO</v-alert>
+</template>
+  
+<script setup lang="ts">
+</script>
---
+++ components/EventDisplay/EventDisplayModeSelector.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn-toggle
+    v-model="modelDisplayMode"
+    color="primary"
+    mandatory
+  >
+
+    <template 
+      v-for="displayMode in displayModes"
+      :key="displayMode[0]"
+    >
+      <v-tooltip :text="displayMode[2]" location="top">
+        <template #activator="{ props }">
+          <v-btn
+            :icon="displayMode[1]" 
+            :value="displayMode[0]" 
+            v-bind="props"
+          />
+        </template>
+      </v-tooltip>
+    </template>
+
+  </v-btn-toggle>
+</template>
+
+<script setup lang="ts">
+  const modelDisplayMode = defineModel<"grid"|"calendar"|"list">("displayMode");
+
+  const displayModes: [value: string, icon: string, tooltip: string][] = [
+    ["grid", "mdi-view-grid-outline", "Display as Grid"],
+    ["list", "mdi-format-list-bulleted", "Display as List"],
+    ["calendar", "mdi-calendar-month", "Display as Calendar"]
+  ];
+
+</script>
