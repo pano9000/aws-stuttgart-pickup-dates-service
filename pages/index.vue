e627c99f8946ac6db9fa9d095ed57c2428b9e2ed
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-25 23:48:06 +0200

    feat(i18n): add i18n to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,15 @@
   <v-container
     class="text-center"
   >
-    <h1>Stuttgart Trash Pickup Dates Service Tool</h1>
-    <p>
-      Unofficial utility web page to help with staying on top of the Trash Pickup Dates from Stuttgart's AWS Abfallwirtschaft
-    </p>
+    <h1>{{ i18n.t("pageHome.pageTitle") }}</h1>
+    <p>{{ i18n.t("pageHome.pageSubtitle") }}</p>
 
     <v-row>
 
       <v-col>
-        <v-card title="How to Use">
+        <v-card :title="i18n.t('pageHome.cardTitleHowToUse')">
           <v-card-text>
-            Select your address by using the Location Picker above, and then click on one of the cards below
+            {{ i18n.t('pageHome.cardTextHowToUse') }}
           </v-card-text>
         </v-card>
       </v-col>
@@ -24,7 +22,7 @@
       <template v-for="actionCard in actionCards" :key="actionCard[0]">
         <v-col cols="12" md="6" lg="auto">
           <NuxtLink :to="actionCard[0]" class="nuxtlink">
-            <v-card :title="actionCard[1]" class="pa-8" color="primary" link>
+            <v-card :title="i18n.t(actionCard[1])" class="pa-8" color="primary" link>
               <v-card-text class="pa-4">
                 <v-icon :icon="actionCard[2]" size="4rem"/>
               </v-card-text>
@@ -38,10 +36,29 @@
 
 <script setup lang="ts">
   import { mdiCalendar, mdiCalendarMultiselect, mdiCalendarExport } from '@mdi/js';
+  const { i18n, multiMergeLocaleMessage } = useCustomI18n();
+  
+  multiMergeLocaleMessage("pageHome", [
+    //@TODO: fix this mess of a title
+    ["pageTitle", {de: "Stuttgart Müll-Abholtermine-Tool", en: "Stuttgart Trash Pickup Dates Service Tool"}],
+    [
+      "pageSubtitle", {
+        de: "Inoffizielle Hilfswebsite, die Ihnen helfen soll, um die Müll-Abholtermine von Stuttgart's AWS Abfallwirtschaft im Überblick zu behalten", 
+        en: "Unofficial utility web page to help with staying on top of the Trash Pickup Dates from Stuttgart's AWS Abfallwirtschaft"
+      }
+    ],
+    ["cardTitleHowToUse", {de: "Verwendung", en: "How To Use"}],
+    ["cardTextHowToUse", {de: "Wählen Sie Ihre Adresse mit dem Addressauswähler oben aus und klicken Sie auf eine der Karten unten.", en: "Select your address by using the Location Picker above, and then click on one of the cards below."}],
+    ["cardTitleUpcomingPickups", {de: "Bevorstehende Abholungen anzeigen", en: "Show Upcoming Pickups"}],
+    ["cardTitleAllPickups", {de: "Alle Abholungen anzeigen", en: "Show All Pickups"}],
+    ["cardTitleCustomIcal", {de: "Eigenen iCal-Link erstellen", en: "Generate Custom iCal Link"}]
+
+  ]);
+
   const actionCards: [url: string, title: string, icon: string][] = [
-    ["/upcoming", "Show Upcoming Pickups", mdiCalendar],
-    ["/all", "Show All Pickups", mdiCalendarMultiselect],
-    ["/icalgenerator", "Generate Custom iCal Link", mdiCalendarExport]
+    ["/upcoming", "pageHome.cardTitleUpcomingPickups", mdiCalendar],
+    ["/all", "pageHome.cardTitleAllPickups", mdiCalendarMultiselect],
+    ["/icalgenerator", "pageHome.cardTitleCustomIcal", mdiCalendarExport]
   ]
 
 
