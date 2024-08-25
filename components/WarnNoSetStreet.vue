97f7b17adeeb2d4f2032c0df50319dd7289f45ac
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-25 19:52:37 +0200

    feat(i18n): add i18n to warnnosetStreet

--- components/WarnNoSetStreet.vue
+++ components/WarnNoSetStreet.vue
@@ -3,11 +3,20 @@
     v-if="!hasSetStreet"
     type="info"
   >
-    Please select your street name and street number first with the location picker.
+    {{ i18n.t("warnNoSetStreet.infoText") }}
   </v-alert>
 </template>
 
 <script setup lang="ts">
   import { useCookieUserConfig } from "~/composables/useCookieUserConfig.ts";
   const { hasSetStreet } = useCookieUserConfig();
+  const { i18n, multiMergeLocaleMessage } = useCustomI18n();
+
+  multiMergeLocaleMessage("warnNoSetStreet", [[
+      "infoText", {
+        de: "Bitte wählen Sie zuerst eine Adresse mit dem Addressauswähler aus.", 
+        en: "Please select an Address with the location picker first."
+      }
+    ]
+  ]);
 </script>
