f4b62b3c7673fa5b1bd0aa69417ebadce3004203
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-28 08:59:12 +0200

    feat(EventDisplayDataIterator): move ItemsPerPage select to own improved component

---
+++ components/EventDisplay/EventDisplayItemsPerPageSelector.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select
+    v-model="modelItemsPerPage"
+    v-bind="tooltipProps"
+    :items="props.items"
+    hide-details
+    type="number"
+    :label="i18n.t('eventDisplayItemsPerPageSelector.tooltip')"
+  />
+</template>
+
+<script setup lang="ts">
+  const modelItemsPerPage = defineModel<number>();
+  const props = defineProps<{
+    items: number[]
+  }>();
+
+  const { i18n, multiMergeLocaleMessage } = useCustomI18n();
+
+  multiMergeLocaleMessage("eventDisplayItemsPerPageSelector", [
+    ["tooltip", {"de": "Einträge pro Seite", "en": "Items per Page"}],
+  ]);
+</script>
