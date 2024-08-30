49eb18ec3f2b2888275386b26918e676b2935197
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-30 08:10:07 +0200

    feat(EventDisplayExportAsMenu): add MenuItem subcomponent

---
+++ components/EventDisplay/EventDisplayExportAsMenuItem.vue
@@ -0,0 +1,19 @@
+<template>
+ <v-list-item>
+    {{ props.title }}
+    <!-- @TODO: check accessibility! -->
+    <DownloadButton :download-url="exportUrl"/>
+    <CopyToClipboardButton :text-to-copy="props.exportUrl"/>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+  import CopyToClipboardButton from "../CopyToClipboardButton.vue";
+  import DownloadButton from "../DownloadButton.vue";
+
+  const props = defineProps<{
+    title: string;
+    exportUrl: string;
+  }>();
+
+</script>
