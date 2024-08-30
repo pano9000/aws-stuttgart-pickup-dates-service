9634d75eec9cd709c964d53ad7c32893a0ec4116
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-30 08:08:29 +0200

    feat(DownloadButton): add reusable download button

---
+++ components/DownloadButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-tooltip :text="i18n.t(`downloadButton.tooltipDownload`)" location="top">
+    <template #activator="{ props: activatorProps }">
+      <v-btn 
+        :icon="mdiDownload"
+        v-bind="activatorProps" 
+        :href="props.downloadUrl"
+        target="_blank"
+      />
+    </template>
+  </v-tooltip>
+</template>
+
+<script setup lang="ts">
+  import { mdiDownload } from "@mdi/js"
+
+  const { i18n, multiMergeLocaleMessage }  = useCustomI18n();
+
+  const props = defineProps<{
+    downloadUrl: string
+  }>();
+
+  multiMergeLocaleMessage("downloadButton", [
+    ["tooltipDownload", {de: "Herunterladen", en: "Download"}],
+  ]);
+
+
+</script>
