ee575a717132c57995742f02944c6216c9748da1
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-31 22:55:38 +0200

    chore(getApiUrl): move option type to exported type

--- utils/getApiUrl.ts
+++ utils/getApiUrl.ts
@@ -2,14 +2,7 @@ import type { AwsApiServiceEventTypeName } from "~/server/services/AwsApiService
 import type { RetrieveDataFacadeFormat } from "~/server/services/RetrieveDataFacade";
 
 
-export default function getApiUrl(options: {
-  endpointName: string,
-  streetname: string,
-  streetno: string,
-  eventTypes: AwsApiServiceEventTypeName[],
-  format: RetrieveDataFacadeFormat,
-  language: string
-}) {
+export default function getApiUrl(options: GetApiUrlOptions) {
   const apiUrl = new URL(options.endpointName, "http://localhost:3000"); //@TODO use runtimeconfig
 
   const apiUrlParams = new URLSearchParams([
@@ -23,4 +16,13 @@ export default function getApiUrl(options: {
   apiUrl.search = apiUrlParams.toString();
 
   return apiUrl.toString()
+}
+
+export type GetApiUrlOptions = {
+  endpointName: string,
+  streetname: string,
+  streetno: string,
+  eventTypes: AwsApiServiceEventTypeName[],
+  format: RetrieveDataFacadeFormat,
+  language: string
 }
