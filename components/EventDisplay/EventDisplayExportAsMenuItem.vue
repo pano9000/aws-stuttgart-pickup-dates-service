f35d6d90e6a3200e8de3ea2c15b3c2cd9425246d
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-08-31 18:01:37 +0200

    feat(EventDisplayExportAsMenuItem): improve CSS styling

--- components/EventDisplay/EventDisplayExportAsMenuItem.vue
+++ components/EventDisplay/EventDisplayExportAsMenuItem.vue
@@ -1,12 +1,18 @@
 <template>
   <v-list-item>
-     {{ props.title }}
-     <!-- @TODO: check accessibility! -->
-     <DownloadButton :download-url="props.exportUrl"/>
-     <CopyToClipboardButton :text-to-copy="props.exportUrl"/>
-     <CustomizeExportButton :export-customizer-component="props.exportCustomizerComponent"/>
-   </v-list-item>
- </template>
+    <div class="d-flex ga-2 py-2 justify-space-between align-center">
+
+      <span>{{ props.title }}</span>
+
+      <div class="export-button-wrap">
+        <!-- @TODO: check accessibility! -->
+        <DownloadButton :download-url="props.exportUrl"/>
+        <CopyToClipboardButton :text-to-copy="props.exportUrl"/>
+        <CustomizeExportButton :export-customizer-component="props.exportCustomizerComponent"/>
+      </div>
+    </div>
+  </v-list-item>
+</template>
  
  <script setup lang="ts">
    import DownloadButton from "../DownloadButton.vue";
@@ -19,3 +25,9 @@
    }>();
 
  </script>
+
+ <style lang="css">
+.export-button-wrap > * {
+  margin: 0rem .25rem;
+}
+ </style>
