9138d5ee8ea79bdd474c2fc39297111718cd9c7f
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-09-02 08:44:59 +0200

    feat(useCookieUserConfig): set default cookie options
    closes #40

--- composables/useCookieUserConfig.ts
+++ composables/useCookieUserConfig.ts
@@ -1,34 +1,39 @@
+import type { CookieOptions } from "#app";
 import type { AwsApiServiceEventTypeName } from "~/server/services/AwsApiService";
+
+function setDefaultCookieSettings<T>(value: T): CookieOptions<T> & { readonly?: false} {
+  return {
+    default: () => {
+      return value
+    },
+    maxAge: 60*60*24*365, // 1 year,
+    sameSite: "strict",
+  }
+}
 
 export function useCookieUserConfig() {
 
   //@TODO can we get rid of the default here?
-  const cookieStreet = useCookie<{streetname: string, streetno: string}>("street", {
-    default: () => {
-      return {
-        streetname: "",
-        streetno: ""
-      }
-    }
-  });
+  const cookieStreet = useCookie("street", setDefaultCookieSettings<{streetname: string, streetno: string}>({
+    streetname: "",
+    streetno: ""
+  }));
 
   const hasSetStreet = computed<boolean>( () => !!cookieStreet.value.streetname && !!cookieStreet.value.streetno);
-  
-  const cookieLanguage = useCookie<string>("lang", { 
-    default: () => "de"
-  });
 
-  const cookieEventDisplayItemsPerPage = useCookie<number>("eventDisplayItemsPerPage", {
-    default: () => 6
-  });
+  const cookieLanguage = useCookie("lang", setDefaultCookieSettings<string>("de"));
 
-  const cookieEventDisplayMode = useCookie<"grid" | "list" | "calendar">("eventDisplayMode", {
-    default: () => "grid"
-  });
+  const cookieEventDisplayItemsPerPage = useCookie("eventDisplayItemsPerPage", 
+    setDefaultCookieSettings<number>(6)
+  );
 
-  const cookieEventTypeSelector = useCookie<AwsApiServiceEventTypeName[]>("eventTypeSelector", {
-    default: () => ["residual", "organic", "recycle", "paper"]
-  });
+  const cookieEventDisplayMode = useCookie("eventDisplayMode",
+    setDefaultCookieSettings<"grid" | "list" | "calendar">("grid")
+  );
+
+  const cookieEventTypeSelector = useCookie("eventTypeSelector", 
+    setDefaultCookieSettings<AwsApiServiceEventTypeName[]>(["residual", "organic", "recycle", "paper"])
+  );
 
   return {
     cookieStreet,
