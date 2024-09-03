0c9d56f246323cb3b1140105877745850afc106f
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-09-03 23:25:15 +0200

    feat(BaseEventDisplay): remove conditional checks -> to be moved to DataIterator

--- components/EventDisplay/BaseEventDisplay.vue
+++ components/EventDisplay/BaseEventDisplay.vue
@@ -1,6 +1,4 @@
 <template>
-  <WarnNoSetStreet/>
-
   <v-container v-if="fetchError">
     <v-alert 
       type="error"
@@ -9,28 +7,16 @@
     />
   </v-container>
 
-  <v-container v-if="eventData">
-
+  <v-container v-else>
     <EventDisplayDataIterator
       :event-data="eventData"
       :is-loading="fetchStatus === 'pending'"
       :api-endpoint="props.apiEndpoint"
-      />
-
-  </v-container>
-
-  <v-container v-else>
-    <v-alert 
-      type="info"
-      :title="i18n.t('baseEventDisplay.fetchNoDataTitle')"
-      :text="i18n.t('baseEventDisplay.fetchNoDataText')"
     />
   </v-container>
-
 </template>
 
 <script setup lang="ts">
-  import WarnNoSetStreet from "../WarnNoSetStreet.vue";
   import EventDisplayDataIterator from "./EventDisplayDataIterator.vue";
   import type { AwsApiServiceResponseAll } from "~/server/services/AwsApiService.js"
 
@@ -69,15 +55,6 @@
       "fetchErrorText", {
         "de": "Bitte prüfen Sie Ihre ausgewählte Addresse und probieren es später erneut oder bitte melden Sie das Problem.", 
         "en": "Please check your selected address and try again later or kindly report the issue."
-      }
-    ],
-    [
-      "fetchNoDataTitle", {"de": "Es gibt nichts zu sehen", "en": "Nothing to see here"}
-    ],
-    [
-      "fetchNoDataText", {
-        "de": "Es sieht so aus, als wenn es keine Daten zum Anzeigen gibt.",
-        "en": "It looks like there is no data to display."
       }
     ],
   ]);
