abdd21f2f5ca08ed330bb529383cd43a1fd452ec
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-11-25 21:26:52 +0100

    feat(CookieNoticeDialog): improve messaging and style of dialog

--- components/CookieNotice/CookieNoticeDialog.vue
+++ components/CookieNotice/CookieNoticeDialog.vue
@@ -3,15 +3,21 @@
     <template #default>
       <v-card class="pa-4">
         <v-card-title>
+          <span><v-icon :icon="mdiCookie"/></span>
           {{ i18n.t("cookieNoticeDialog.title") }}
         </v-card-title>
         <v-card-text>
-         {{ i18n.t("cookieNoticeDialog.text") }}
-          <ul style="list-style-position: inside;" class="mt-4">
-            <li v-for="entry in ['address', 'language', 'pickupPreference', 'displayPreference']" :key="entry">
+         <p>
+           {{ i18n.t("cookieNoticeDialog.text") }}
+         </p>
+          <ul style="list-style-position: inside;" class="my-4">
+            <li v-for="entry in ['address', 'pickupPreference', 'language', 'displayPreference']" :key="entry">
               {{ i18n.t(`cookieNoticeDialog.${entry}`) }}
             </li>
           </ul>
+          <p>
+            {{ i18n.t("cookieNoticeDialog.cookieDelete") }}
+          </p>
         </v-card-text>
 
         <v-card-actions>
@@ -24,7 +30,7 @@
 </template>
 
 <script setup lang="ts">
-  import { mdiCheck } from "@mdi/js";
+  import { mdiCheck, mdiCookie } from "@mdi/js";
 
   const modelActive = defineModel<boolean>("active");
   const { i18n, multiMergeLocaleMessage } = useCustomI18n();
@@ -32,12 +38,16 @@
   multiMergeLocaleMessage("cookieNoticeDialog", [
     ["title", {de: "Informationen über eingesetzte Cookies", en: "Information about Cookies in use"}],
     ["text", {
-      de: "Diese Seite verwendet ausschließlich funktionale Cookies, um folgende Einstellungen zu speichern:", 
-      en: "This site utilizes exclusively functional cookies, to store the following settings:"}
+      de: "Diese Webseite verwendet ausschließlich funktionale Cookies, um Ihnen die Verwendung der Website zu ermöglichen. Hierzu werden folgende Daten als Cookie lokal in ihrem Browser gespeichert:",
+      en: "This website utilizes exclusively functional cookies, to make the site useable for you. For this prupose the following data are stored as cookies locally in your browser:"}
     ],
-    ["address", {de: "Adresse", en: "Address"}],
-    ["language", {de: "Sprache", en: "Language"}],
+    ["address", {de: "Abholadresse und -hausnummer", en: "Pickup Address and Street Number"}],
     ["pickupPreference", {de: "Bevorzugte Abholarten", en: "Pickup Type Preferences"}],
+    ["language", {de: "Spracheinstellungen", en: "Language Settings"}],
     ["displayPreference", {de: "Ansichtseinstellungen", en: "Display Preferences"}],
+    ["cookieDelete", {
+      de: "Sie können diese Daten löschen lassen, in dem Sie die Funktion 'Cookies löschen' Ihres Browsers verwenden.",
+      en: "You can delete this data by using your browser's 'Clear Cookies' function."
+    }]
   ]);
 </script>
