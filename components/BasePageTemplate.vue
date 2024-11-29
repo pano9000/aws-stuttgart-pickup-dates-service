3ce89995e69911c91b8d87a46b13352a6d977d6c
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-11-29 09:03:02 +0100

    feat(BasePageTemplate): add basic reusable template

---
+++ components/BasePageTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container
+    tag="main"
+  >
+    <header class="mb-16 text-center">
+      <h1 class="text-h3 mb-4">{{ props.pageTitle }}</h1>
+      <p v-if="props.pageSubtitle" class="text-h5 mb-4">{{ props.pageSubtitle }}</p>
+      <slot name="headerAfterSubtitle"/>
+    </header>
+
+    <section>
+      <slot name="mainContent"/>
+    </section>
+
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+  pageTitle: string;
+  pageSubtitle?: string;
+}>();
+
+</script>
+
+<style>
+
+</style>
