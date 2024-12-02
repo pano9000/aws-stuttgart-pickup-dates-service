910454c1e6dde9b51f1073d3f38da684d443fe26
Panagiotis Papadopoulos <pano_90@gmx.net>
2024-12-02 08:43:42 +0100

    feat(pages/impressum): improve content

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,8 +1,11 @@
 <template>
 
-  <BasePageTemplate :page-title="i18n.t('legalNotice.pageTitle')">
+  <BasePageTemplate
+    :page-title="i18n.t('legalNotice.pageTitle')"
+    :page-subtitle="i18n.t('legalNotice.pageSubtitle')"
+  >
     <template #mainContent>
-      <address class="text-left">
+      <address class="text-center">
         <p v-for="(line) in legalNoticeLines" :key="line">
           {{ runtimeConfig.public[line] }}
         </p>
@@ -15,13 +18,13 @@
 <script setup lang="ts">
   import BasePageTemplate from '~/components/BasePageTemplate.vue';
 
-
   const { i18n, multiMergeLocaleMessage }  = useCustomI18n();
   const runtimeConfig = useRuntimeConfig();
   const legalNoticeLines = Object.keys(runtimeConfig.public).filter(key => key.startsWith("legalNotice"))
 
   multiMergeLocaleMessage("legalNotice", [
-    ["pageTitle", {de: "Impressum", en: "Legal Notice"}],
+    ["pageTitle", {de: "Impressum", en: "Legal Notice / Impressum"}],
+    ["pageSubtitle", {de: "Angaben gemäß § 5 DDG", en: "Information in accordance with § 5 DDG"}]
   ]);
 
 </script>
